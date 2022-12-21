cf2c4f3f3e85a9a8a0bb68dc79afa3de8494bf98
Marcel Reig <marcelreig@gmail.com>
2022-12-21 18:47:41 +0100

    edit clock

--- src/components/FunctionalComponent.jsx
+++ src/components/FunctionalComponent.jsx
@@ -1,46 +1,45 @@
 import React, { useState, useEffect } from "react";
 
 const FunctionalClock = () => {
-  const [newUser, setNewUser] = useState({
+  const defaultState = {
     fecha: new Date(),
     edad: 0,
     nombre: "Marcel",
     apellidos: "Reig Ibarra",
-  });
+  };
+
+  const [user, setUser] = useState(defaultState);
 
   useEffect(() => {
-    timerID = setInterval(() => tick(), 1000);
-  });
-
-  useEffect(() => {
+    const intervalAge = setInterval(() => {
+      actualiceUser();
+    }, 1000);
     return () => {
-      clearInterval(timerID);
+      clearInterval(intervalAge);
     };
-  }, []);
+  });
+
+  const actualiceUser = () => {
+    return setUser({
+      fecha: user.fecha,
+      edad: user.edad + 1,
+      nombre: user.nombre,
+      apellidos: user.apellidos,
+    });
+  };
 
   return (
     <div>
       <h2>
         Hora Actual:
-        {fecha.toLocaleTimeString()}
+        {user.fecha.toLocaleTimeString()}
       </h2>
       <h3>
-        {nombre} {apellidos}
+        {user.nombre} {user.apellidos}
       </h3>
-      <h1>Edad: {edad}</h1>
+      <h1>Edad: {user.edad}</h1>
     </div>
   );
-
-  function tick() {
-    setNewUser((prevState) => {
-      let edad = prevState.edad + 1;
-      return {
-        ...prevState,
-        fecha: new Date(),
-        edad,
-      };
-    });
-  }
 };
 
 export default FunctionalClock;
